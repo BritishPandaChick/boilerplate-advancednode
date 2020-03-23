68e73d8bb48c1e462e6d47d44a7093dd03ee7f3c
BritishPandaChick <sbartley1080@gmail.com>
2020-03-23 18:14:50 -0400

    Add code for How to use Passport strategies.

--- server.js
+++ server.js
@@ -59,8 +59,18 @@ mongo.connect(process.env.DATABASE, (err, db) => {
 
       app.route('/')
         .get((req, res) => {
-          res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+          res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'login', showLogin: true});
         });
+      
+      app.route('/login')
+        .post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+          res.redirect('/profile');
+      });
+
+      app.route('/profile')
+        .get((req, res) => {
+          res.render(process.cwd() + '/views/pug/profile');
+      }); 
 
       app.listen(process.env.PORT || 3000, () => {
         console.log("Listening on port " + process.env.PORT);
