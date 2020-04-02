1cd842b5f00777db50a67f22fb28ad8adcf53c93
BritishPandaChick <sbartley1080@gmail.com>
2020-04-02 02:05:58 -0400

    Add registration of users code

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongo.connect(process.env.DATABASE, (err, db) => {
+mongo.connect(process.env.DATABASE, { useUnifiedTopology: true }, (err, db) => {
   if(err) {
       console.log('Database error: ' + err);
   } else {
@@ -66,7 +66,7 @@ mongo.connect(process.env.DATABASE, (err, db) => {
 
       app.route('/')
         .get((req, res) => {
-          res.render(process.cwd() + '/views/pug/index', {title: 'Home page', message: 'login', showLogin: true});
+          res.render(process.cwd() + '/views/pug/index', {title: 'Home page', message: 'login', showLogin: true, showRegistration: true});
         });
       
       app.route('/login')
@@ -78,6 +78,31 @@ mongo.connect(process.env.DATABASE, (err, db) => {
         .get(ensureAuthenticated, (req, res) => {
           res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
       }); 
+
+      app.route('/register')
+        .post((req, res, next) => {
+          db.collection('users').findOne({ username: req.body.username }, function(err, user){
+            if(err){
+              next(err);
+            } else if(user) {
+              res.redirect('/');
+            } else {
+              db.collection('users').insertOne({
+                username: req.body.username,
+                password: req.body.password}, 
+                (err, doc) => {
+                  if(err){
+                    res.redirect('/');
+                  } else {
+                    next(null, user);
+                  }
+                })
+            }
+          })
+      }, passport.authenticate('local', { failureRedirect: '/' }), 
+          (req, res, next) => {
+            res.redirect('/profile');
+          });
 
       app.route('/logout')
         .get((req, res) => {
